7c58fd198c0f81b7a028ee0e3f71a8802b7c5403
Kazuki Tsunemi <ksilverwall@gmail.com>
2020-03-07 02:31:47 +0900

    Refactor auth module

--- get-user-info/index.js
+++ get-user-info/index.js
@@ -1,40 +1,9 @@
 const Auth = require('thankshell-libs/auth.js');
-const AWS = require("aws-sdk");
-
-// FIXME: Register to auth module
-const getUser = async(dynamo, userId) => {
-    if (!userId) {
-        return {
-            status: 'UNREGISTERED',
-        }
-    }
-
-    let result = await dynamo.get({
-        TableName: process.env.USERS_TABLE_NAME,
-        Key:{
-            'user_id': userId,
-        },
-    }).promise();
-
-    if (result.Item) {
-        return {
-            status: result.Item.status,
-            user_id: result.Item.user_id,
-            displayName: result.Item.display_name ? result.Item.display_name : result.Item.user_id,
-        };
-    } else {
-        return {
-            status: 'UNREGISTERED',
-            'user_id': userId,
-        };
-    }
-}
 
 const run = async(event) => {
-    const dynamo = new AWS.DynamoDB.DocumentClient()
     const userId = await Auth.getUserId(event.requestContext.authorizer.claims)
 
-    return await getUser(dynamo, userId)
+    return await Auth.getUser(userId)
 };
 
 exports.handler = async (event, context, callback) => {
