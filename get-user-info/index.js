23ef4e6160b5f90f549c33b7f39c6e7e5ab5624e
Kazuki Tsunemi <ksilverwall@gmail.com>
2020-03-07 01:35:19 +0900

    Refactor get-user-info

--- get-user-info/index.js
+++ get-user-info/index.js
@@ -1,31 +1,12 @@
-let Auth = require('thankshell-libs/auth.js');
-let AWS = require("aws-sdk");
+const Auth = require('thankshell-libs/auth.js');
+const AWS = require("aws-sdk");
 
 // FIXME: Register to auth module
-let getUserInfo = async(claims) => {
-    let userId;
-    let dynamo = new AWS.DynamoDB.DocumentClient();
-
-    if (claims.identities) {
-        let identities = JSON.parse(claims.identities);
-
-        let authId = identities.providerName + ':' + identities.userId;
-        let result = await dynamo.get({
-            TableName: process.env.AUTH_TABLE_NAME,
-            Key:{
-                'auth_id': authId,
-            },
-        }).promise();
-
-        if (!result.Item) {
-            return {
-                status: 'UNREGISTERED',
-            };
+const getUser = async(dynamo, userId) => {
+    if (!userId) {
+        return {
+            status: 'UNREGISTERED',
         }
-
-        userId = result.Item['user_id'];
-    } else {
-        userId = claims['cognito:username'];
     }
 
     let result = await dynamo.get({
@@ -43,23 +24,23 @@ let getUserInfo = async(claims) => {
             'user_id': userId,
         };
     }
-};
+}
 
-let getUsetInfo = async(event) => {
-    return {
-        statusCode: 200,
-        data: await getUserInfo(event.requestContext.authorizer.claims),
-    };
+const run = async(event) => {
+    const dynamo = new AWS.DynamoDB.DocumentClient()
+    const userId = await Auth.getUserId(event.requestContext.authorizer.claims)
+
+    return await getUser(dynamo, userId)
 };
 
 exports.handler = async (event, context, callback) => {
     try {
-        let result = await getUsetInfo(event);
+        const result = await run(event);
 
         return {
-            statusCode: result.statusCode,
+            statusCode: 200,
             headers: {"Access-Control-Allow-Origin": "*"},
-            body: JSON.stringify(result.data),
+            body: JSON.stringify(result),
         };
     } catch(err) {
         console.log(err);
