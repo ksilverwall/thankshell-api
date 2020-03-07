c69a1eb2f14ee16e3c43ed80a04cc0e0d781dd13
Kazuki Tsunemi <ksilverwall@gmail.com>
2020-03-08 00:06:26 +0900

    Extend to get member display name

--- get-group-info/index.js
+++ get-group-info/index.js
@@ -1,87 +1,54 @@
-let Auth = require('thankshell-libs/auth.js');
-let AWS = require("aws-sdk");
+const Auth = require('thankshell-libs/auth.js');
+const AWS = require("aws-sdk");
 
-async function getHistory(dynamo, account, stage) {
-    let tableName = {
-        'production': {
-            'info': 'table_info',
-            'data': 'remittance_transactions',
-        },
-        'develop': {
-            'info': 'dev_table_info',
-            'data': 'dev_remittance_transactions',
-        },
-    };
-
-    let adminMode = false;
-
-    let history = {
-        Count: 0,
-        Items: []
-    };
-
-    let tableInfo = await dynamo.get({
-        TableName: tableName[stage]['info'],
-        Key:{
-            'name': tableName[stage]['data'],
-        }
-    }).promise();
-
-    let maxBlockId = tableInfo.Item ? Math.floor(tableInfo.Item.current_id_sequence / 1000) : 0;
-
-    for (let blockId=maxBlockId; blockId >= 0; --blockId) {
-        let params;
-        if(adminMode) {
-            params = {
-                TableName: tableName[stage]['data'],
-                KeyConditionExpression: "block_id = :block",
-                ExpressionAttributeValues: {
-                    ":block": blockId
-                }
-            };
-        } else {
-            params = {
-                TableName: tableName[stage]['data'],
-                KeyConditionExpression: "block_id = :block",
-                ExpressionAttributeValues: {
-                    ":block": blockId,
-                }
-            };
-        }
-
-        var data = await dynamo.query(params).promise();
-        history.Items = history.Items.concat(data.Items);
-        history.Count += data.Count;
-    }
-
-    return history;
-}
-
-let getTransactions = async(event) => {
-    let userInfo = await Auth.getUserInfo(event.requestContext.authorizer.claims);
-    let gid = event.pathParameters.group;
-
-    let dynamo = new AWS.DynamoDB.DocumentClient();
-    let groupInfo = await dynamo.get({
+const getGroup = async(dynamo, groupId) => {
+    const result = await dynamo.get({
         TableName: process.env.GROUPS_TABLE_NAME,
         Key:{
-            'group_id': gid,
+            'group_id': groupId,
         },
-    }).promise();
-    
+    }).promise()
+
+    return result.Item
+}
+
+const getMemberDetails = async(memberIds) => {
+    const allMembers = await Promise.all(memberIds.map(userId => Auth.getUser(userId)))
+
+    let dict = {}
+    allMembers.forEach(user => {
+        if (user.status === 'ENABLE') {
+            dict[user.user_id] = {displayName: user.displayName}
+        }
+    })
+
+    return dict
+}
+const run = async(event) => {
+    const dynamo = new AWS.DynamoDB.DocumentClient()
+    const groupId = event.pathParameters.group
+
+    const userId = await Auth.getUserId(event.requestContext.authorizer.claims)
+    const group = await getGroup(dynamo, groupId)
+
+    if (!group.members.values.includes(userId)) {
+        new Error('user is not a member')
+    }
+
+    const memberDetails = await getMemberDetails(group.members.values)
     return {
-        statusCode: 200,
-        data: groupInfo.Item,
-    };
-};
+        ...group,
+        memberDetails: memberDetails,
+    }
+}
 
 exports.handler = async(event, context, callback) => {
     try {
-        let result = await getTransactions(event);
+        const result = await run(event)
         return {
-            statusCode: result.statusCode,
+            statusCode: 200,
             headers: {"Access-Control-Allow-Origin": "*"},
-            body: JSON.stringify(result.data),
+            body: JSON.stringify(result),
         };
     } catch(err) {
         console.log(err);
@@ -91,6 +58,6 @@ exports.handler = async(event, context, callback) => {
             body: JSON.stringify({
                 'message': err.message,
             }),
-        };
+        }
     }
-};
+}
