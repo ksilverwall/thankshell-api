c94c1ab76349f47608a95c842a34edb3d698deb0
Kazuki Tsunemi <ksilverwall@gmail.com>
2020-04-04 03:54:03 +0900

    Add joined joined groups to user

--- get-user-info/index.js
+++ get-user-info/index.js
@@ -1,9 +1,35 @@
 const Auth = require('thankshell-libs/auth.js');
+const AWS = require("aws-sdk")
+
+const getJoinedGroups = async(userId) => {
+    const client = new AWS.DynamoDB.DocumentClient()
+
+    const data = await client.scan({
+        TableName: process.env.GROUPS_TABLE_NAME,
+        ProjectionExpression: "group_id",
+        FilterExpression: "contains(#members, :user_id)",
+        ExpressionAttributeNames:{
+            '#members': 'members'
+        },
+        ExpressionAttributeValues: {
+            ":user_id": userId,
+        }
+    }).promise()
+
+    return data.Items.map(record => record.group_id)
+}
 
 const run = async(event) => {
     const userId = await Auth.getUserId(event.requestContext.authorizer.claims)
 
-    return await Auth.getUser(userId)
+    const user = await Auth.getUser(userId)
+
+    const groups = await getJoinedGroups(userId)
+
+    return {
+        ...user,
+        groups: groups,
+    }
 };
 
 exports.handler = async (event, context, callback) => {
