258ec83889b40a3cb4abded177c0a7eb2deb7cf5
Kazuki Tsunemi <ksilverwall@gmail.com>
2020-05-31 01:43:11 +0900

    Refactor update-user

--- update-user/index.js
+++ update-user/index.js
@@ -1,62 +1,36 @@
 const Auth = require('thankshell-libs/auth.js');
-const AWS = require("aws-sdk");
+const appInterface = require('thankshell-libs/interface.js');
+const GroupMembersDao = require('thankshell-libs/GroupMembersDao.js');
 
-class UserError extends Error {}
-
-const updateUser = async(userId, user) => {
-    console.log(`Update user: ${userId}`)
-    console.log(`Update data: ${user}`)
-
-    const dynamo = new AWS.DynamoDB.DocumentClient();
-
-    await dynamo.update({
-        TableName: process.env.USERS_TABLE_NAME,
-        Key:{
-            'user_id': userId,
-        },
-        UpdateExpression: 'SET display_name = :display_name',
-        ExpressionAttributeValues: {
-            ':display_name': user.displayName,
-        },
-    }).promise()
-}
 
 const run = async(event) => {
-    console.log(`EventBody: ${event.body}`)
-    console.log(`PathParameter: ${event.pathParameters}`)
+  console.log(`EventBody: ${event.body}`);
+  console.log(`PathParameter: ${event.pathParameters}`);
 
-    const userId = event.pathParameters.userId
-    if (!userId) {
-        throw new UserError('userId is not set in path parameters')
-    }
-    if (userId !== await Auth.getUserId(event.requestContext.authorizer.claims)) {
-        throw new UserError('No access permission')
-    }
+  const groupId = 'sla';
+  const userId = event.pathParameters.userId
+  const authId = Auth.getAuthId(event.requestContext.authorizer.claims);
+  const member = JSON.parse(event.body)
 
-    const eventBody = JSON.parse(event.body)
+  if (!userId) {
+    throw new appInterface.ApplicationError(
+      "userId is not set in path parameters",
+      "INVALID_PARAMTER",
+      400
+    );
+  }
 
-    await updateUser(userId, eventBody)
-}
+  await GroupMembersDao.updateAsync(groupId, authId, userId, member);
+};
 
-exports.handler = async(event, context, callback) => {
-    try {
-        console.log(event)
-        await run(event)
+exports.handler = async(event) => {
+  try {
+    const result = await run(event);
 
-        return {
-            statusCode: 200,
-            headers: {"Access-Control-Allow-Origin": "*"},
-            body: JSON.stringify({}),
-        }
-    } catch(err) {
-        console.log(err);
+    return appInterface.getSuccessResponse(result);
+  } catch(err) {
+    console.error(err);
 
-        return {
-            statusCode: 500,
-            headers: {"Access-Control-Allow-Origin": "*"},
-            body: JSON.stringify({
-                'message': err.message,
-            }),
-        }
-    }
-}
+    return appInterface.getErrorResponse(err);
+  }
+};
