16f768e7536e63fd8fde15b9c620c0ea104bca41
Kazuki Tsunemi <ksilverwall@gmail.com>
2020-05-31 02:37:04 +0900

    Refactor get-user-info

--- get-user-info/index.js
+++ get-user-info/index.js
@@ -1,55 +1,22 @@
 const Auth = require('thankshell-libs/auth.js');
-const AWS = require("aws-sdk")
+const appInterface = require('thankshell-libs/interface.js');
+const UserController = require('thankshell-libs/UserController.js');
 
-const getJoinedGroups = async(userId) => {
-    const client = new AWS.DynamoDB.DocumentClient()
-
-    const data = await client.scan({
-        TableName: process.env.GROUPS_TABLE_NAME,
-        ProjectionExpression: "group_id",
-        FilterExpression: "contains(#members, :user_id)",
-        ExpressionAttributeNames:{
-            '#members': 'members'
-        },
-        ExpressionAttributeValues: {
-            ":user_id": userId,
-        }
-    }).promise()
-
-    return data.Items.map(record => record.group_id)
-}
 
 const run = async(event) => {
-    const userId = await Auth.getUserId(event.requestContext.authorizer.claims)
+  const authId = Auth.getAuthId(event.requestContext.authorizer.claims);
 
-    const user = await Auth.getUser(userId)
-
-    const groups = await getJoinedGroups(userId)
-
-    return {
-        ...user,
-        groups: groups,
-    }
+  return await (new UserController(authId)).getAsync();
 };
 
-exports.handler = async (event, context, callback) => {
-    try {
-        const result = await run(event);
+exports.handler = async(event) => {
+  try {
+    const result = await run(event);
 
-        return {
-            statusCode: 200,
-            headers: {"Access-Control-Allow-Origin": "*"},
-            body: JSON.stringify(result),
-        };
-    } catch(err) {
-        console.log(err);
+    return appInterface.getSuccessResponse(result);
+  } catch(err) {
+    console.error(err);
 
-        return {
-            statusCode: 500,
-            headers: {"Access-Control-Allow-Origin": "*"},
-            body: JSON.stringify({
-                "message": err.message,
-            }),
-        };
-    }
+    return appInterface.getErrorResponse(err);
+  }
 };
