482bb842bf7967fc12fc0f4afb83285c8a0a1fc2
Kazuki Tsunemi <ksilverwall@gmail.com>
2020-08-16 23:37:13 +0900

    Add unit test framework

---
+++ test/functions/create_transactions/index.test.js
@@ -0,0 +1,12 @@
+const prodict = require('../../../src/functions/create_transaction/index.js');
+
+const assert = require('assert');
+
+describe('Simple Math Test', () => {
+ it('should return 2', () => {
+        assert.equal(1 + 1, 2);
+    });
+ it('should return 9', () => {
+        assert.equal(3 * 3, 9);
+    });
+});
